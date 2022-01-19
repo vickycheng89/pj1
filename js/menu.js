a1cf0399e6995b1c0a3d02508492d4bb26a2ab67
vickycheng89 <vickycheng89@gmail.com>
2022-01-19 15:58:28 +0800

    v1-3

--- js/menu.js
+++ js/menu.js
@@ -7,4 +7,23 @@ let menuHtml=`
 //取得DOM的節點
 let menu=document.getElementById('menu');
 //menu.textContent = menuHtml//寫入節點文字內容
-menu.innerHTML = menuHtml;
+menu.innerHTML = menuHtml;//HTML寫入節點
+
+let menuData=[
+    {
+        title:'首頁',
+        url:'index.html'
+    },
+    {
+        title:'關於我們',
+        url:'about.html'
+    },
+    {
+        title:'商品展示',
+        url:'products.html'
+    },
+    {
+        title:'購物車',
+        url:'store.html'
+    },
+];
